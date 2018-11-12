cd4e95d6c44125c07c873a6fb98d006f444b1b1b
nicolasvp <nicolasverapalominos@gmail.com>
2018-11-12 23:33:41 +0100

    pom

---
+++ target/classes/import.sql
@@ -0,0 +1,18 @@
+INSERT INTO countries (id,name,created_at) VALUES('1','Chile','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('2','Argentina','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('3','Brasil','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('4','Uruguay','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('5','Finlandia','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('6','Alemania','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('7','Australia','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('8','Nueva Zelanda','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('9','Ucrania','2018-10-28');
+INSERT INTO countries (id,name,created_at) VALUES('10','Holanda','2018-10-28');
+
+INSERT INTO genres (id,name,created_at) VALUES('1','Hombre','2018-10-28');
+INSERT INTO genres (id,name,created_at) VALUES('2','Mujer','2018-10-28');
+INSERT INTO genres (id,name,created_at) VALUES('3','Otro','2018-10-28');
+
+INSERT INTO roles (id,name,description,created_at) VALUES('1','SuperAdmin','Super administrador con todos los permisos','2018-10-28');
+INSERT INTO roles (id,name,description,created_at) VALUES('2','Admin','Administrador con casi todos los permisos','2018-10-28');
+INSERT INTO roles (id,name,description,created_at) VALUES('3','User','Usuario con permisos muy limitados','2018-10-28');
